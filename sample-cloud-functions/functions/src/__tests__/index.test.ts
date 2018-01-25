ecdcd1e52642b712d6ebc2516ede857bce126420
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-01-25 23:31:02 +0900

    fix test

--- sample-cloud-functions/functions/src/__tests__/index.test.ts
+++ sample-cloud-functions/functions/src/__tests__/index.test.ts
@@ -1,31 +1,47 @@
 import * as admin from 'firebase-admin'
 import 'jest'
+import * as Orderable from '@star__hoshi/orderable'
+// import { Pring } from 'pring'
 
 beforeAll(() => {
   const serviceAccount = require('../../sandbox-329fc-firebase-adminsdk.json')
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   })
+
+  // Pring.initialize({
+  //   projectId: 'sandbox-329fc',
+  //   keyFilename: './sandbox-329fc-firebase-adminsdk.json'
+  // })
+
+  Orderable.initialize({
+    adminOptions: {
+      projectId: 'sandbox-329fc',
+      keyFilename: './sandbox-329fc-firebase-adminsdk.json'
+    }, stripeToken: '', slack: { url: '', channel: '' }
+  })
 })
 
 it('order create', async () => {
-  jest.setTimeout(1000000)
+  // jest.setTimeout(1000000)
 
-  const skuData = [...Array(3).keys()].map(index => {
-    return { price: 5000, stock: 1000, index: index }
-  })
-  const skus = await Promise.all(skuData.map(data => {
-    return admin.firestore().collection('version/1/testsku').add(data)
-  }))
+  // const skuData = [...Array(3).keys()].map(index => {
+  //   return { price: 5000, stock: 1000, index: index }
+  // })
+  // const skus = await Promise.all(skuData.map(data => {
+  //   return admin.firestore().collection('version/1/testsku').add(data)
+  // }))
 
-  const testOrders = [...Array(5).keys()].map(a => {
-    const testOrder: any = {}
-    testOrder.orderSKUs = skus
-    return testOrder
-  })
+  // const testOrders = [...Array(5).keys()].map(a => {
+  //   const testOrder: any = {}
+  //   testOrder.orderSKUs = skus
+  //   return testOrder
+  // })
 
-  // await addOrdersPer02(testOrders)
-  await addOrdersAtOnce(testOrders)
+  // // await addOrdersPer02(testOrders)
+  // await addOrdersAtOnce(testOrders)
+  const aaa = new Orderable.Model.User()
+  await aaa.save()
 
   expect(true)
 })
